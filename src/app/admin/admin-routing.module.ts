b15d17b3cccf8354b0e4f0b777e850f5af04e11f
pertvirt <justforplaydota2@gmail.com>
2020-02-07 02:15:11 +0700

    Update 2:14 AM

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -6,12 +6,13 @@ import {ProjectListComponent} from './project-list/project-list.component';
 import {ProjectCreateComponent} from './project-create/project-create.component';
 import {ProjectUpdateComponent} from './project-update/project-update.component';
 import {LoginComponent} from './login/login.component';
+import {AdminGuard} from './admin.guard';
 
 const routes: Routes = [
   {path: 'admin', component: ProjectComponent, children: [
-      {path: 'list', component: ProjectListComponent},
-      {path: 'create', component: ProjectCreateComponent},
-      {path: 'update', component: ProjectUpdateComponent},
+      {path: 'list', component: ProjectListComponent, canActivate: [AdminGuard]},
+      {path: 'create', component: ProjectCreateComponent, canActivate: [AdminGuard]},
+      {path: 'update', component: ProjectUpdateComponent, canActivate: [AdminGuard]},
       {path: 'login', component: LoginComponent}
     ]
   }
