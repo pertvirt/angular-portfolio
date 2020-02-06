81188b8d6a514cc0388ac5aed796cb517dde3a08
pertvirt <justforplaydota2@gmail.com>
2020-02-06 14:09:30 +0700

    Upload 2:09PM

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,12 +5,14 @@ import {ProjectComponent} from './project/project.component';
 import {ProjectListComponent} from './project-list/project-list.component';
 import {ProjectCreateComponent} from './project-create/project-create.component';
 import {ProjectUpdateComponent} from './project-update/project-update.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {path: 'admin', component: ProjectComponent, children: [
       {path: 'list', component: ProjectListComponent},
       {path: 'create', component: ProjectCreateComponent},
-      {path: 'update', component: ProjectUpdateComponent}
+      {path: 'update', component: ProjectUpdateComponent},
+      {path: 'login', component: LoginComponent}
     ]
   }
 ];
@@ -19,5 +21,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
+export class AdminRoutingModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,20 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AdminModule} from './admin/admin.module';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAUZsmcna5K_PSgX05YHUYZg9bd3hBLDGg',
+  authDomain: 'angular-portfolio-9d0d9.firebaseapp.com',
+  databaseURL: 'https://angular-portfolio-9d0d9.firebaseio.com',
+  projectId: 'angular-portfolio-9d0d9',
+  storageBucket: 'angular-portfolio-9d0d9.appspot.com',
+  messagingSenderId: '889380303337',
+  appId: '1:889380303337:web:515770c67ea64329005b78',
+  measurementId: 'G-J0Y8FYRY31'
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +40,9 @@ import {AdminModule} from './admin/admin.module';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    AdminModule
+    AdminModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
